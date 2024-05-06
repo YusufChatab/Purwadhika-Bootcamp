8d1a5383195747c303d35bb6dff603a1bfbc72b5
Muhammad Yusuf Chatab <ychatab@gmail.com>
2024-05-06 23:01:23 +0800

    added session 4

---
+++ Introduction To Web Development/Session 4/array.js
@@ -0,0 +1,20 @@
+let arr = ["A", "B", "C", "D", "E"];
+console.log(arr);
+
+let scores = [10, 20, 30, 40, 50];
+console.log(scores);
+
+let kotak = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+// console.log(arr.toString());
+arr.pop();
+console.log(arr);
+
+arr.push("F");
+console.log(arr);
+
+console.log(arr.length);
