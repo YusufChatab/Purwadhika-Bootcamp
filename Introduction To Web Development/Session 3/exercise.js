cab289883ea8f03dff11ba1a7533b59ae66eff81
Muhammad Yusuf Chatab <ychatab@gmail.com>
2024-05-06 21:17:59 +0800

    revised exercised day 3

--- Introduction To Web Development/Session 3/exercise.js
+++ Introduction To Web Development/Session 3/exercise.js
@@ -2,19 +2,40 @@ const input = 9;
 const limit = 10;
 
 for (let i = 1; i <= limit; i++) {
-  console.log(`${input} x ${limit} = ${input * i}`);
+  console.log(`${input} x ${i} = ${input * i}`);
 }
 
-function palindrome() {
-  let str = "madam";
+let word = "madam";
+let reverseWord = "";
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] !== str[str.length - 1 - i]) {
-      console.log("Not a palindrome");
-    } else console.log("Palindrome");
+for (let i = word.length - 1; i >= 0; i--) {
+  reverseWord += word.charAt(i);
+  console.log(reverseWord);
+}
+
+if (word == reverseWord) {
+  console.log("Palindrome");
+} else {
+  console.log("Is Not Palindrome");
+}
+
+// console.log(reverseWord);
+
+let rupiah = 1234567;
+console.log("Rp " + rupiah.toLocaleString("id-ID"));
+
+// let helloWorld = "Hello World";
+// let searchWorld = "ell";
+// // let result = helloWorld.replace(searchWorld, "");
+
+let string = "hello world";
+let result = "";
+
+for (i = 0; i <= string.length; i++) {
+  if (i == 0 || string.charAt(i - 1) == " ") {
+    result += string.charAt(i).toUpperCase();
+  } else {
+    result += string.charAt(i).toLowerCase();
   }
 }
-
-let rupiah = 1000;
-
-console.log("Rp " + rupiah.toLocaleString("id-ID"));
+console.log(result);
