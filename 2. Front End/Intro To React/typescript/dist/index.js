141c9fe42a84d600fc41e5a7dad252aced7f22ab
Muhammad Yusuf Chatab <ychatab@gmail.com>
2024-05-22 23:15:21 +0800

    added intro to react and typescript

---
+++ 2. Front End/Intro To React/typescript/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let x = 2;
+let y = 10;
+function add(a, b) {
+    return a + b;
+}
+console.log(add(x, y));
---
+++ 2. Front End/Intro To React/typescript/index.ts
@@ -0,0 +1,8 @@
+let x: number = 2;
+let y: number = 10;
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(add(x, y));
